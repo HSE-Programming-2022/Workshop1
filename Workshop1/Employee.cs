c54db8591bb08e87c14aa1712b13d4f573a1ee60
arendarsky <arendarskydd@gmail.com>
2022-01-11 13:47:29 +0300

    first commit

---
+++ Workshop1/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop1.Test
+{
+    public class Employee
+    {
+        public string Name;
+        public double Salary;
+    }
+}
---
+++ Workshop1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Workshop1.Test;
+
+
+namespace Workshop1
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+
+            string salaryStr = Console.ReadLine();
+            double salary = Double.Parse(salaryStr);
+            bool isForeigner = bool.Parse(Console.ReadLine());
+
+            Employee employee = new Employee()
+            {
+                Name = name,
+                Salary = salary
+            };
+
+            double salaryAfterTax = 0;
+            if (isForeigner)
+            {
+                salaryAfterTax = employee.Salary * (1 - 0.3);
+            }
+            else
+            {
+                salaryAfterTax = employee.Salary * (1 - 0.13);
+            }
+
+            Console.WriteLine(salaryAfterTax);
+
+            Console.ReadKey();
+        }
+    }
+}
